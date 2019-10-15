dc59a384dba702fcdbd0e729c5c91319b6e4a380
Jasdeep Singh <jasdeep.singh@comprotechnologies.com>
2019-10-15 18:23:51 +0530

    updating item reader

--- items/api.items.js
+++ items/api.items.js
@@ -14,7 +14,9 @@ class ItemsAPI{
         try{
           let promise =  this.httpModule.get(this.hostUrl + '/items/' + itemId + '?denormalize=true&mode=undefined');
           return new Promise(function(resolve, reject){
-              promise.then(response => JSON.parse(response.responseText));
+              promise.then( function(response){
+                return JSON.parse(response.responseText)
+              });
           });
 
         }
